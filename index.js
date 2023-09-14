f2f40616b5d0ca8312e717d35ea265ff47401cc0
Elite6277 <hacklinux93@gmail.com>
2023-09-14 18:03:45 +0500

    1_TypeScript_Базовые_типы_данных

---
+++ index.js
@@ -0,0 +1,13 @@
+var userName = "";
+userName.isInteger();
+userName = 5;
+// string '', "", ``
+// number 10, 0.5, 0.001, -50, 4e10
+// boolean true, false
+var isBirthday = true;
+var age;
+var userName = "John";
+age = 40;
+if (isBirthday) {
+    console.log("Congrats ".concat(userName.toUpperCase(), ", age: ").concat(age + 1));
+}
---
+++ index.ts
@@ -0,0 +1,18 @@
+let userName: string = "";
+userName.isInteger();
+
+userName = 5;
+
+// string '', "", ``
+// number 10, 0.5, 0.001, -50, 4e10
+// boolean true, false
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = "John";
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+}

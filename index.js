4a0c400eb8bcc65fb86153518edb3f82612bdd0c
Elite6277 <hacklinux93@gmail.com>
2023-09-14 18:51:52 +0500

    4_TypeScript_Практика типизации кода

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var isBirthdayData = true;
-var ageData = 40;
-var userNameData = "John";
-function logBrtMsg(isBirthday, userName, age) {
-    if (isBirthday) {
-        console.log("Congrats ".concat(userName.toUpperCase(), ", age: ").concat(age + 1));
+//4 Практика типизации кода
+var currRate = "1.05";
+var fetchCurr = function (response) {
+    var data = JSON.parse(response);
+    return data;
+};
+function transferEurToUsd(available, amount, commission) {
+    if (available) {
+        var res = fetchCurr(currRate) * amount * commission;
+        console.log(res);
+        // Или запись в элемент на странице вместо консоли
+    }
+    else {
+        console.log("Сейчас обмен недоступен");
     }
 }
-logBrtMsg(isBirthdayData, userNameData, ageData);
+transferEurToUsd(true, 500, 1.05);
--- index.ts
+++ index.ts
@@ -1,38 +1,24 @@
-// 3_Специальный тип any
+//4 Практика типизации кода
 
-// ! Таких записей стоит избегать
-// let salary;
-// salary = 5000;
+const currRate: string = "1.05";
 
-// ! Вот проавильный вариант
-let salary: number;
-salary = 5000;
+const fetchCurr = (response: string) => {
+  const data = JSON.parse(response);
+  return data;
+};
 
-//! Главный совет который поможет при работе с TypeScript старайтесь избегать этого типа, чем его больше в TypeScript тем больше потенциальных багов и меньше пользы от самого языка, ведь если тип может быть любым то какой смысл от системы типов
-
-//! Сейчас мы разберем несколько примеров когда any может появиться  и что с ним делать в таком случае
-
-//! Тип any это зло
-
-//! Бывают ли ситуации когда нужно самому использовать any? да бывают но крайне крайне редко, так что старайтесь всегда избегать any вообще в любых ситуациях, так ваш код будет куда безопаснее, а значит и вас избавит от лишних проблем
-
-const userData =
-  '{"isBirthdayData": true, "ageData": 40, "userNameData": "John}';
-
-const userObj: {
-  isBirthdayData: boolean;
-  userNameData: string;
-  ageData: number;
-} = JSON.parse(userData);
-
-console.log(userObj.smt());
-
-function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-  if (isBirthday) {
-    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+function transferEurToUsd(
+  available: boolean,
+  amount: number,
+  commission: number
+): void {
+  if (available) {
+    let res = fetchCurr(currRate) * amount * commission;
+    console.log(res);
+    // Или запись в элемент на странице вместо консоли
   } else {
-    return "Error";
+    console.log("Сейчас обмен недоступен");
   }
 }
 
-logBrtMsg();
+transferEurToUsd(true, 500, 1.05);

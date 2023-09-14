11b73634daee5a980db015430bfb15ea25525afa
Elite6277 <hacklinux93@gmail.com>
2023-09-14 18:45:19 +0500

    2_ypecript_Использование системы типов в функциях

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var userName = "";
-userName.isInteger();
-userName = 5;
-// string '', "", ``
-// number 10, 0.5, 0.001, -50, 4e10
-// boolean true, false
-var isBirthday = true;
-var age;
-var userName = "John";
-age = 40;
-if (isBirthday) {
-    console.log("Congrats ".concat(userName.toUpperCase(), ", age: ").concat(age + 1));
+var isBirthdayData = true;
+var ageData = 40;
+var userNameData = "John";
+function logBrtMsg(isBirthday, userName, age) {
+    if (isBirthday) {
+        console.log("Congrats ".concat(userName.toUpperCase(), ", age: ").concat(age + 1));
+    }
 }
+logBrtMsg(isBirthdayData, userNameData, ageData);
--- index.ts
+++ index.ts
@@ -1,18 +1,44 @@
-let userName: string = "";
-userName.isInteger();
+//  2_TypeScript_Использование системы типов в функциях
+const isBirthdayData: boolean = true;
+let ageData: number = 40;
+const userNameData: string = "John";
 
-userName = 5;
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): void {
+//   if (isBirthday) {
+//     console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+//   }
 
-// string '', "", ``
-// number 10, 0.5, 0.001, -50, 4e10
-// boolean true, false
+//   return undefined;
+// }
 
-const isBirthday: boolean = true;
-let age: number;
-const userName: string = "John";
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+//   } else {
+//     return "Error";
+//   }
+// }
+const logBrtMsg = (
+  isBirthday: boolean,
+  userName: string,
+  age: number
+): string => {
+  if (isBirthday) {
+    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+  } else {
+    return "Error";
+  }
+};
 
-age = 40;
+logBrtMsg(isBirthdayData, userNameData, 40);
 
-if (isBirthday) {
-  console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
-}
+// ? Если говорить простыми словами то void после функции значит что ничего не возвращяет viod это пустотта нечего
+// ? Любая функция что то возвращяет даже если это нигде не указано он по defaultу вернет нам undefined
+
+// ? В TypeScript есть механизм который позволяет  нам указывать что именно будет возвращять функция, и если вдруг она логически ничего не возвращяет то можно подставить viod что и делает type script самостоятельно но при возможности нужно это прописывать самому
+
+// ? Void это не только отсутствие возвращяемого значения но и его полное игнорирование это полезно при совмещении некоторых функций
+
+// ? Возможна диструктуризация  объекта внутри  аргумента функции как это часто делается в реальности
+
+// ? По возможности всегда нужно указывать тип возвращяемого значения функции, когда тип возврящяемого значения явно указан намного проще и удобнее читать любую функцию
